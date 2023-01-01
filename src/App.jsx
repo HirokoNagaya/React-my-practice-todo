eacb5cecee203ac52c0e35fa8a15b0f64b71352a
nagaya_hiroko(divx) <hiroko.nagaya@divx.co.jp>
2023-01-01 16:18:30 +0900

    [add]基本的な構造の作成、CSSの付与
    what/アプリの基本的な構造の作成
    why/ボタンやplaceholderなど基本的な構造を作るため

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,40 @@ import './App.css';
 import React from 'react';
 
 export const App = () => {
-  return
-    <div></div>
-
+  return (
+  <>
+    <div className="input-area">
+      <input placeholder="TODOを入力" />
+      <button>追加</button>
+    </div>
+    <div className="incomplete-area">
+      <p className="title">未完了のTODO</p>
+        <div className="list-row">
+          <ul>
+          <li>あああああ</li>
+          <button>完了</button>
+          <button>削除</button>
+          </ul>
+        </div>
+        <div>
+          <ul>
+          <li>いいいいい</li>
+          <button>完了</button>
+          <button>削除</button>
+          </ul>
+        </div>
+    </div>
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+        <div className="list-row">
+          <ul>
+          <li>ううううう</li>
+          <button>完了</button>
+          <button>削除</button>
+          </ul>
+        </div>
+    </div>
+  </>
+  );
 };
 
