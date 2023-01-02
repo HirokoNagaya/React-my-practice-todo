ed3abe292a1dd04c067d3a7a9f12c866e7370d4b
nagaya_hiroko(divx) <hiroko.nagaya@divx.co.jp>
2023-01-02 17:02:31 +0900

    [add]styleの当て方の変更
    what/styleの当て方の変更
    why/Reactのstyleの当て方をするため

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  borderRadius: "8px",
+  padding: "8px",
+  margin: "8px"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
     <input placeholder="TODOを入力"
     value={todoText}
     onChange={onChange}
