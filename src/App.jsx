5f23093540e4b4fff09eb737da111861a3b1f46b
nagaya_hiroko(divx) <hiroko.nagaya@divx.co.jp>
2023-01-02 14:54:31 +0900

    [add]タスクの完了機能
    what/タスクの完了機能の実装
    why/完了ボタンを押すと完了のTODO欄に項目が移動するようにするため

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,15 @@ export const App = () => {
     setIncompleteTodos(newTodos);
   };
 
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
   return (
   <>
     <div className="input-area">
@@ -40,7 +49,7 @@ export const App = () => {
               return (
                 <div key={todo} className="list-row">
                   <li>{todo}</li>
-                  <button>完了</button>
+                  <button onClick={() => onClickComplete(index)}>完了</button>
                   <button onClick={() => onClickDelete(index)}>削除</button>
                 </div>
               );
@@ -52,8 +61,7 @@ export const App = () => {
         return(
         <div key={todo} className="list-row">
           <li>{todo}</li>
-          <button>完了</button>
-          <button>削除</button>
+          <button>戻す</button>
         </div>
         );
       })}
