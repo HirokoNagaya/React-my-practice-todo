57940041119a84dad96f066d7538c91764b625cf
nagaya_hiroko(divx) <hiroko.nagaya@divx.co.jp>
2023-01-02 14:13:20 +0900

    [add]タスクの追加機能
    what/タスクの追加機能の実装
    why/入力された項目を未完了のTODOリストに入れるため

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { useState } from 'react';
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState([
     "あああああ",
     "いいいいい"
   ]);
   const [completeTodos, setCompleteTodos] = useState(["ううううう"]);
+
+  const onChangeTodotext = (event) => setTodoText(event.target.value);
+
+  const onClickadd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
   <>
     <div className="input-area">
-      <input placeholder="TODOを入力" />
-      <button>追加</button>
+      <input placeholder="TODOを入力"
+      value={todoText}
+      onChange={onChangeTodotext}
+      />
+      <button onClick={onClickadd}>追加</button>
     </div>
     <div className="incomplete-area">
       <p className="title">未完了のTODO</p>
