0825b68992314c7089a737c9e42b416c40097522
nagaya_hiroko(divx) <hiroko.nagaya@divx.co.jp>
2023-01-02 14:38:40 +0900

    [add]削除機能の実装
    what/削除機能の実装
    why/項目の削除機能をつけるため

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,12 @@ export const App = () => {
     setTodoText("");
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
   <>
     <div className="input-area">
@@ -30,12 +36,12 @@ export const App = () => {
     </div>
     <div className="incomplete-area">
       <p className="title">未完了のTODO</p>
-            {incompleteTodos.map((todo) => {
+            {incompleteTodos.map((todo, index) => {
               return (
                 <div key={todo} className="list-row">
                   <li>{todo}</li>
                   <button>完了</button>
-                  <button>削除</button>
+                  <button onClick={() => onClickDelete(index)}>削除</button>
                 </div>
               );
             })}
